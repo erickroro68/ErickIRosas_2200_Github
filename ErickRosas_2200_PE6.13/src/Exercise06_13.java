0d3a69cf1437eb3c5b377c5286fa51617b9955a5
rosas <rosas@LAPTOP-LQ8BVF3G.lan>
2022-11-07 11:15:54 -0600

    Adding more whitespaces to the title to make it look more organized

--- ErickRosas_2200_PE6.13/src/Exercise06_13.java
+++ ErickRosas_2200_PE6.13/src/Exercise06_13.java
@@ -10,7 +10,9 @@ public class Exercise06_13 {
 	public static void main(String[] args) {
 		
 		//4s%20s\n is the number of whitespaces we see in the code
+		//Creating the 
 		System.out.printf("%4s%20s\n","i","m(i)");
+		System.out.printf("%5s%19s\n","---","------");
 		//still need a loop
 		//max loop of 20
 		for(int i = 1; i <= 20; i++) {
